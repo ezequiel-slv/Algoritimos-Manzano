893c6ec10250a33c7bdd2c31fe3bbd65282cfbf7
Ezequiel <ezequielsilvalima01@gmail.com>
2024-09-15 22:08:06 -0300

    commit

---
+++ exe15.py
@@ -0,0 +1,12 @@
+#Elaborar um programa que leia quatro valores numéricos inteiros (variáveis A, B, C e D). Ao final, o programa deve apresentar o resultado, armazenado em memória, do produto (variável P) do primeiro com o terceiro valor, e o resultado da soma (variável S) do segundo com o quarto valor.
+
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+D = int(input("Digite o valor de D: "))
+
+P = (A - C)
+S = (B + D)
+
+print(f"O resultado do produto de A e C é: {P} e o resultado de ")
+
